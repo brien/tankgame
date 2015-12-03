10b9d790e225690b3847db309f3133a05db01800
Brien Smith Martínez <bsmartinez@gmail.com>
2015-12-03 11:56:55 +0100

    Started xcode project

---
+++ tankgame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tankgame
+//
+//  Created by Brien Smith Martínez on 3/12/15.
+//  Copyright © 2015 Brien Smith Martínez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
