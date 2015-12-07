343a394d88611b6733ff1f6256a85ac7bbf1ef04
Brien Smith Martínez <bsmartinez@gmail.com>
2015-12-07 21:44:57 +0100

    Added Singleton.h

---
+++ tankgame/Singleton.h
@@ -0,0 +1,13 @@
+//
+//  Singleton.h
+//  tankgame
+//
+//  Created by Brien Smith Martínez on 7/12/15.
+//  Copyright © 2015 Brien Smith Martínez. All rights reserved.
+//
+
+#ifndef Singleton_h
+#define Singleton_h
+
+
+#endif /* Singleton_h */
