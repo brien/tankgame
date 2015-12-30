a98bb9e3d41e5cd35e7f7265eb56be4b2f6cc961
Brien Smith Martínez <bsmartinez@gmail.com>
2015-12-30 17:37:02 -0600

    Code formatting

--- tankgame/TankHandler.h
+++ tankgame/TankHandler.h
@@ -7,43 +7,43 @@
 class TankHandler : public Singleton<TankHandler>
 {
 public:
-	TankHandler();
-	~TankHandler();
-
-	void Init();
+    TankHandler();
+    ~TankHandler();
     
-   Tank players[4];
-   float combo[2];
-   float special[2];
-   int comboNum[2];
-   int hitCombo[2];
-   int wins[2];
-   //nk player2;
-
-	vector<Tank> tanks;
-
-	void CreateTank();
-	void DestroyTank();
-
-	void NextFrame();
-
-	void DrawTanks();
-	void DrawBullets();
-
-	float closest;
-
-   bool isInputJoy;
-
-   int diff;
-
-   //bool twop;
-   int numPlayers;
-   int attackingTanks;
-   
-   igtl_QGLMesh bodymesh;
-   igtl_QGLMesh turretmesh;
-   igtl_QGLMesh cannonmesh;
-
+    void Init();
+    
+    Tank players[4];
+    float combo[2];
+    float special[2];
+    int comboNum[2];
+    int hitCombo[2];
+    int wins[2];
+    //nk player2;
+    
+    vector<Tank> tanks;
+    
+    void CreateTank();
+    void DestroyTank();
+    
+    void NextFrame();
+    
+    void DrawTanks();
+    void DrawBullets();
+    
+    float closest;
+    
+    bool isInputJoy;
+    
+    int diff;
+    
+    //bool twop;
+    int numPlayers;
+    int attackingTanks;
+    
+    igtl_QGLMesh bodymesh;
+    igtl_QGLMesh turretmesh;
+    igtl_QGLMesh cannonmesh;
+    
 };
 
 #endif
