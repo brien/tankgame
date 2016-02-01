584990ae41d030070302312940aabc0c98c1dbe8
Brien Smith Martínez <bsmartinez@gmail.com>
2016-02-01 12:12:21 +0100

    Added error checking in VideoTask, and default values for settings

--- tankgame/VideoTask.cpp
+++ tankgame/VideoTask.cpp
@@ -3,6 +3,7 @@
 #include "App.h"
 
 #include <SDL2/SDL.h>
+#include <iostream>
 
 
 int VideoTask::scrWidth=800;
@@ -25,6 +26,7 @@ bool VideoTask::Start()
     
     if(-1==SDL_InitSubSystem(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK))
     {
+        std::cerr << "VideoTask::Start: SDL_InitSubSystem failed." << std::endl;
         return false;
     }
     SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 2 );
@@ -42,61 +44,75 @@ bool VideoTask::Start()
     
     char line[64];
     FILE *filein;
-    filein=fopen("settings.txt", "rt");
-    
-    fgets(line, 64, filein);
-    fgets(line, 64, filein);
-    
-    if(line[0]=='0')
+    filein=fopen("./settings.txt", "rt");
+    if(!filein)
     {
+        std::cerr << "VideoTask::Start: Failed to open settings.txt" << std::endl;
+        std::cerr << "VideoTask::Start: Using default settings" << std::endl;
+        
         flags = SDL_WINDOW_OPENGL;
-        //hide the mouse cursor
-        //SDL_ShowCursor(SDL_DISABLE);
+        
+        scrWidth=1280;
+        scrHeight=720;
+        
+        TankHandler::GetSingleton().diff=0;
+        App::GetSingleton().soundTask->disable=false;
     }
     else
     {
-        flags = SDL_WINDOW_OPENGL|SDL_WINDOW_FULLSCREEN;
-        //hide the mouse cursor
-        SDL_ShowCursor(SDL_DISABLE);
-        SDL_SetRelativeMouseMode(SDL_TRUE);
+        fgets(line, 64, filein);
+        fgets(line, 64, filein);
+        
+        if(line[0]=='0')
+        {
+            flags = SDL_WINDOW_OPENGL;
+            //hide the mouse cursor
+            //SDL_ShowCursor(SDL_DISABLE);
+        }
+        else
+        {
+            flags = SDL_WINDOW_OPENGL|SDL_WINDOW_FULLSCREEN;
+            //hide the mouse cursor
+            SDL_ShowCursor(SDL_DISABLE);
+            SDL_SetRelativeMouseMode(SDL_TRUE);
+        }
+        
+        fgets(line, 64, filein);
+        fgets(line, 64, filein);
+        
+        scrWidth=(line[0]-48)*100;
+        scrWidth+=(line[1]-48)*10;
+        scrWidth+=(line[2]-48);
+        
+        if(line[3]>=48 && line[3]<=57)
+        {
+            scrWidth*=10;
+            scrWidth+=(line[3]-48);
+        }
+        
+        fgets(line, 64, filein);
+        fgets(line, 64, filein);
+        
+        scrHeight=(line[0]-48)*100;
+        scrHeight+=(line[1]-48)*10;
+        scrHeight+=(line[2]-48);
+        
+        if(line[3]>=48 && line[3]<=57)
+        {
+            scrHeight*=10;
+            scrHeight+=(line[3]-48);
+        }
+        
+        fgets(line, 64, filein);
+        fgets(line, 64, filein);
+        
+        TankHandler::GetSingleton().diff=(line[0]-48);
+        
+        fgets(line, 64, filein);
+        fgets(line, 64, filein);
+        
+        App::GetSingleton().soundTask->disable=(bool)(line[0]-48);
     }
-    
-    
-    fgets(line, 64, filein);
-    fgets(line, 64, filein);
-    
-    scrWidth=(line[0]-48)*100;
-    scrWidth+=(line[1]-48)*10;
-    scrWidth+=(line[2]-48);
-    
-    if(line[3]>=48 && line[3]<=57)
-    {
-        scrWidth*=10;
-        scrWidth+=(line[3]-48);
-    }
-    
-    fgets(line, 64, filein);
-    fgets(line, 64, filein);
-    
-    scrHeight=(line[0]-48)*100;
-    scrHeight+=(line[1]-48)*10;
-    scrHeight+=(line[2]-48);
-    
-    if(line[3]>=48 && line[3]<=57)
-    {
-        scrHeight*=10;
-        scrHeight+=(line[3]-48);
-    }
-    
-    fgets(line, 64, filein);
-    fgets(line, 64, filein);
-    
-    TankHandler::GetSingleton().diff=(line[0]-48);
-    
-    fgets(line, 64, filein);
-    fgets(line, 64, filein);
-    
-    App::GetSingleton().soundTask->disable=(bool)(line[0]-48);
     
     fclose(filein);
     
@@ -108,6 +124,7 @@ bool VideoTask::Start()
     
     if(!window)
     {
+        std::cerr << "VideoTask::Start: SDL_CreateWindow failed." << std::endl;
         return false;
     }
     
