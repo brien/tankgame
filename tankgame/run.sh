9134bdac66262f2bbccd977eeea298e2d4871ef8
Brien Smith Martínez <bsmartinez@gmail.com>
2020-07-23 20:52:02 +0200

    Add run shell script

---
+++ tankgame/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#  run.sh
+#  tankgame
+#
+#  Created by Brien Smith Martínez on 23/7/20.
+#  Copyright © 2020 Brien Smith Martínez. All rights reserved.
---
+++ tankgame/runtime/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  run.sh
+#  tankgame
+#
+#  Created by Brien Smith Martínez on 23/7/20.
+#  Copyright © 2020 Brien Smith Martínez. All rights reserved.
+
+./tankgame
