ddd56b69e40684c9c5a90db2decaa16aae5ee773
Brien Smith Martinez <bsmartinez@gmail.com>
2023-03-01 19:30:04 +0100

    Remove commented code.

--- tankgame/Bullet.cpp
+++ tankgame/Bullet.cpp
@@ -611,7 +611,6 @@ void Bullet::Draw()
         
         glScalef(1, 1, 0.15);
         
-        //glCallList(App::GetSingleton().graphicsTask->squarelist2);
         App::GetSingleton().graphicsTask->squarelist2.Call(0);
 
         glEnable(GL_BLEND);
