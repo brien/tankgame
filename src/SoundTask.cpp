e51b427ba282cc2916a731a52899a8bd71d0efc3
Brien Smith Martinez <bsmartinez@gmail.com>
2025-05-06 19:35:12 +0200

    Code formatting for SountTask class

--- src/SoundTask.cpp
+++ src/SoundTask.cpp
@@ -2,7 +2,6 @@
 //
 //////////////////////////////////////////////////////////////////////
 
-
 #include "SoundTask.h"
 #include <iostream>
 #include <SDL2/SDL.h>
@@ -15,175 +14,171 @@ unsigned int SoundTask::samplingRate = 44100;
 
 SoundTask::SoundTask()
 {
-    disable=false;
+    disable = false;
 }
 
 SoundTask::~SoundTask()
 {
-    
 }
 
 bool SoundTask::Start()
 {
-    if(!disable)
+    if (!disable)
     {
-        music=NULL;
-        energy=NULL;
-        cowbell=NULL;
+        music = NULL;
+        energy = NULL;
+        cowbell = NULL;
         xphone = NULL;
-        fire=NULL;
-        
-        jump=NULL;
-        land=NULL;
-        
-        death=NULL;
-        death2=NULL;
-        
-        hit=NULL;
-        bounce=NULL;
-        
+        fire = NULL;
+
+        jump = NULL;
+        land = NULL;
+
+        death = NULL;
+        death2 = NULL;
+
+        hit = NULL;
+        bounce = NULL;
+
         cowbellChannel = 0;
         xphoneChannel = 1;
-        fireChannel=2;
-        energyChannel=3;
-        
-        jumpChannel=4;
-        landChannel=5;
-        
+        fireChannel = 2;
+        energyChannel = 3;
+
+        jumpChannel = 4;
+        landChannel = 5;
+
         deathChannel = 6;
         death2Channel = 7;
         hitChannel = 8;
         bounceChannel = 9;
-        
+
         audio_rate = 22050;
         audio_format = AUDIO_S16;
         audio_channels = 2;
         audio_buffers = 2048;
-        
-        if(-1==SDL_InitSubSystem(SDL_INIT_AUDIO))
+
+        if (-1 == SDL_InitSubSystem(SDL_INIT_AUDIO))
         {
             return false;
         }
-        
-        if(Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers))
+
+        if (Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers))
         {
             printf("Unable to open audio!\n");
             exit(1);
         }
-        
+
         Mix_AllocateChannels(16);
         Mix_QuerySpec(&audio_rate, &audio_format, &audio_channels);
-        
+
         music = Mix_LoadMUS("sounds/TGTest3.ogg");
-        
+
         cowbell = Mix_LoadWAV("sounds/cowbell.wav");
-        
+
         death = Mix_LoadWAV("sounds/shatter.wav");
         death2 = Mix_LoadWAV("sounds/shatter2.wav");
         hit = Mix_LoadWAV("sounds/hit3.wav");
-        
+
         fire = Mix_LoadWAV("sounds/shot.wav");
         xphone = Mix_LoadWAV("sounds/xphone.wav");
         energy = Mix_LoadWAV("sounds/gain.wav");
-        
+
         jump = Mix_LoadWAV("sounds/jump.wav");
         land = Mix_LoadWAV("sounds/land.wav");
         bounce = Mix_LoadWAV("sounds/elastik.wav");
-        
+
         Mix_Volume(-1, 50);
     }
-    
+
     return true;
 }
 
 void SoundTask::OnSuspend()
 {
-    
 }
 
 void SoundTask::Update()
 {
-    //we don't need to do anything, SDL_mixer does the work
+    // we don't need to do anything, SDL_mixer does the work
 }
 
 void SoundTask::OnResume()
 {
-    
 }
 
 void SoundTask::PlayChannel(int ID)
 {
-    if(!disable)
+    if (!disable)
     {
-        if(ID==cowbellChannel)
+        if (ID == cowbellChannel)
         {
             Mix_PlayChannel(-1, cowbell, 0);
         }
-        if(ID==fireChannel)
+        if (ID == fireChannel)
         {
             Mix_PlayChannel(ID, fire, 0);
         }
-        if(ID==xphoneChannel)
+        if (ID == xphoneChannel)
         {
             Mix_PlayChannel(-1, xphone, 0);
         }
-        if(ID==energyChannel)
+        if (ID == energyChannel)
         {
             Mix_PlayChannel(-1, energy, 0);
         }
-        if(ID==jumpChannel)
+        if (ID == jumpChannel)
         {
             Mix_PlayChannel(-1, jump, 0);
         }
-        if(ID==landChannel)
+        if (ID == landChannel)
         {
             Mix_PlayChannel(-1, land, 0);
         }
-        if(ID==deathChannel)
+        if (ID == deathChannel)
         {
             Mix_PlayChannel(-1, death, 0);
         }
-        if(ID==death2Channel)
+        if (ID == death2Channel)
         {
             Mix_PlayChannel(-1, death2, 0);
         }
-        if(ID==hitChannel)
+        if (ID == hitChannel)
         {
             Mix_PlayChannel(-1, hit, 0);
         }
-        if(ID==bounceChannel)
+        if (ID == bounceChannel)
         {
             Mix_PlayChannel(ID, bounce, 0);
         }
     }
-    
 }
 
 void SoundTask::PlayMusic(int ID)
 {
-    if(ID<0)
+    if (ID < 0)
     {
-        ID=musicID+1;
+        ID = musicID + 1;
     }
-    if(!disable && Mix_PausedMusic() == 0)
+    if (!disable && Mix_PausedMusic() == 0)
     {
-        if(ID>7)
-            ID=0;
-        if(ID == 0)
+        if (ID > 7)
+            ID = 0;
+        if (ID == 0)
         {
             Mix_FreeMusic(music);
             music = Mix_LoadMUS("sounds/TGTest3.ogg");
             Mix_PlayMusic(music, -1);
-            musicID=ID;
+            musicID = ID;
         }
     }
 }
 
 void SoundTask::PauseMusic()
 {
-    if(!disable)
+    if (!disable)
     {
-        if(Mix_PausedMusic() == 0)
+        if (Mix_PausedMusic() == 0)
         {
             Mix_PauseMusic();
         }
@@ -194,10 +189,9 @@ void SoundTask::PauseMusic()
     }
 }
 
-
 void SoundTask::Stop()
 {
-    if(!disable)
+    if (!disable)
     {
         Mix_FreeChunk(cowbell);
         Mix_FreeChunk(fire);
@@ -208,11 +202,11 @@ void SoundTask::Stop()
         Mix_FreeChunk(death2);
         Mix_FreeChunk(hit);
         Mix_FreeChunk(bounce);
-        
+
         Mix_HaltMusic();
         Mix_FreeMusic(music);
         music = NULL;
-        
+
         Mix_CloseAudio();
     }
 }
--- src/SoundTask.h
+++ src/SoundTask.h
@@ -13,56 +13,55 @@ class SoundTask : public ITask
 public:
     SoundTask();
     virtual ~SoundTask();
-    
+
     bool Start();
     void OnSuspend();
     void Update();
     void OnResume();
     void Stop();
-    
+
     void PlayChannel(int ID);
     void PlayMusic(int ID);
     void PauseMusic();
-    
+
     Mix_Music *music;
-    
+
     Mix_Chunk *cowbell;
     Mix_Chunk *fire;
     Mix_Chunk *xphone;
     Mix_Chunk *energy;
-    
+
     Mix_Chunk *jump;
     Mix_Chunk *land;
-    
+
     Mix_Chunk *death;
     Mix_Chunk *death2;
     Mix_Chunk *hit;
     Mix_Chunk *bounce;
-    
+
     int audio_rate;
     Uint16 audio_format;
     int audio_channels;
     int audio_buffers;
-    
+
     int cowbellChannel;
     int fireChannel;
     int xphoneChannel;
     int energyChannel;
-    
+
     int jumpChannel;
     int landChannel;
-    
+
     int deathChannel;
     int death2Channel;
     int hitChannel;
     int bounceChannel;
-    
+
     static unsigned int samplingRate;
-    
+
     bool disable;
-    
+
     int musicID;
-    
 };
 
 #endif
