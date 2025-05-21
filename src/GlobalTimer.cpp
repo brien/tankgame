f1f9ce6a5e62833012147ea726e131849ce22f67
Brien Smith Martinez <bsmartinez@gmail.com>
2025-05-21 18:03:04 +0200

    Code formatting on GlobalTimer

--- src/GlobalTimer.cpp
+++ src/GlobalTimer.cpp
@@ -7,18 +7,16 @@
 #include "GlobalTimer.h"
 #include <SDL2/SDL.h>
 
-float GlobalTimer::dT=0;
-unsigned long GlobalTimer::lastFrameIndex=0;
-unsigned long GlobalTimer::thisFrameIndex=0;
+float GlobalTimer::dT = 0;
+unsigned long GlobalTimer::lastFrameIndex = 0;
+unsigned long GlobalTimer::thisFrameIndex = 0;
 
 GlobalTimer::GlobalTimer()
 {
-    
 }
 
 GlobalTimer::~GlobalTimer()
 {
-    
 }
 
 bool GlobalTimer::Start()
@@ -29,20 +27,19 @@ bool GlobalTimer::Start()
 
 void GlobalTimer::Update()
 {
-    lastFrameIndex=thisFrameIndex;
-    thisFrameIndex=SDL_GetTicks();
-    dT=((float)(thisFrameIndex-lastFrameIndex))/1000.0f;
-    dT*=1.0f;
+    lastFrameIndex = thisFrameIndex;
+    thisFrameIndex = SDL_GetTicks();
+    dT = ((float)(thisFrameIndex - lastFrameIndex)) / 1000.0f;
+    dT *= 1.0f;
 }
 
 void GlobalTimer::Reset()
 {
-    thisFrameIndex=SDL_GetTicks();
-    lastFrameIndex=thisFrameIndex;
-    dT=0;
+    thisFrameIndex = SDL_GetTicks();
+    lastFrameIndex = thisFrameIndex;
+    dT = 0;
 }
 
 void GlobalTimer::Stop()
 {
-    
 }
--- src/GlobalTimer.h
+++ src/GlobalTimer.h
@@ -14,20 +14,19 @@ class GlobalTimer : public ITask
 public:
     GlobalTimer();
     virtual ~GlobalTimer();
-    
-    
+
     static float dT;
     static unsigned long lastFrameIndex;
     static unsigned long thisFrameIndex;
-    
+
     bool Start();
     void Update();
     void Stop();
-    
+
     void Reset();
-    
-    static inline unsigned long TimeToIndex(float t) { return (unsigned long)(t*1000.0f); }
-    static inline float IndexToTime(unsigned long i) { return ((float)i)/1000.0f;	      }
+
+    static inline unsigned long TimeToIndex(float t) { return (unsigned long)(t * 1000.0f); }
+    static inline float IndexToTime(unsigned long i) { return ((float)i) / 1000.0f; }
 };
 
 #endif /* GlobalTimer_h */
