1f35f02eeaaf7d76ead9b79118ac568895c5656f
Brien Smith Martinez <bsmartinez@gmail.com>
2025-05-23 07:46:43 +0200

    Code formatting for LevelHandler class

--- src/LevelHandler.h
+++ src/LevelHandler.h
@@ -17,29 +17,29 @@ class LevelHandler : public Singleton<LevelHandler>
 public:
     LevelHandler();
     ~LevelHandler();
-    
+
     vector<Item> items;
-    
+
     void Init();
-    
+
     bool Load(const char filepath[]);
-    
+
     bool drawFloor;
     bool drawWalls;
     bool drawTop;
 
     int start[2];
     int enemy[16][2];
-    
+
     char fileName[18];
     int sizeX;
     int sizeZ;
-    
+
     void Flatten(int height);
     bool PointCollision(float x, float y, float z);
     void ItemCollision();
     void AddItem(float x, float y, float z, TankType type);
-    
+
     bool HandlePointCollision(float &x, float &y, float &z, float &vx, float &vz);
     bool FallCollision(float x, float y, float z);
     bool FloatCollision(float x, float y, float z);
@@ -47,17 +47,16 @@ public:
     int GetFloatHeight(int x, int z);
     bool SetTerrainHeight(int x, int z, int height);
     void GenerateTerrain();
-    
+
     void DrawTerrain();
     void DrawItems();
-    
+
     void DrawTerrain_OLD();
-    
-    
-    int Strlen(const char * stringy);
-    
+
+    int Strlen(const char *stringy);
+
     void NextLevel(bool forb);
-    
+
     int levelNumber;
     int colorNumber;
     int colorNumber2;
