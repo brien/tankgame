5143a099964bc8b68f4aafda84a086be5056a790
Brien Smith Martinez <bsmartinez@gmail.com>
2025-05-28 18:12:37 +0200

    Code formatting on application entry point

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,16 @@
 //
 //
 #ifdef _WIN32
-    // If building in windows:
-    #include <windows.h>
-    #include <GL/gl.h>
+// If building in windows:
+#include <windows.h>
+#include <GL/gl.h>
 #elif __APPLE__
-    // If building on macOS:
-    #include <OpenGL/gl.h>
-    #include <mach-o/dyld.h>
+// If building on macOS:
+#include <OpenGL/gl.h>
+#include <mach-o/dyld.h>
 #else
-    // If building on Linux:
-    #include <GL/gl.h>
+// If building on Linux:
+#include <GL/gl.h>
 #endif
 
 #include <iostream>
@@ -30,18 +30,18 @@
 
 void App::Run(int argc, char *argv[])
 {
-    if(!Logger::Get().Init())return;
+    if (!Logger::Get().Init())
+        return;
 
     SDL_version compiled;
     SDL_version linked;
-    
+
     SDL_VERSION(&compiled);
     SDL_GetVersion(&linked);
     printf("We compiled against SDL version %d.%d.%d ...\n",
            compiled.major, compiled.minor, compiled.patch);
     printf("But we are linking against SDL version %d.%d.%d.\n",
            linked.major, linked.minor, linked.patch);
-    
 
     videoTask = new VideoTask;
     graphicsTask = new GraphicsTask;
@@ -49,53 +49,52 @@ void App::Run(int argc, char *argv[])
     gameTask = new GameTask;
     globalTimer = new GlobalTimer;
     inputTask = new InputTask;
-    
+
     new TankHandler();
     new LevelHandler();
     new FXHandler();
-    
+
     new TaskHandler();
-    
+
     videoTask->priority = 100;
     TaskHandler::GetSingleton().AddTask(videoTask);
-    
+
     inputTask->priority = 90;
     TaskHandler::GetSingleton().AddTask(inputTask);
-    
+
     graphicsTask->priority = 80;
     TaskHandler::GetSingleton().AddTask(graphicsTask);
-    
+
     soundTask->priority = 70;
     TaskHandler::GetSingleton().AddTask(soundTask);
-    
+
     gameTask->priority = 60;
     TaskHandler::GetSingleton().AddTask(gameTask);
-    
+
     globalTimer->priority = 10;
     TaskHandler::GetSingleton().AddTask(globalTimer);
 
     gameTask->OnResume();
-    
-    quit=false;
-    
+
+    quit = false;
+
     soundTask->PlayMusic(0);
-    
+
     Logger::Get().Write("Initialization complete. About to enter TaskHandler Execute Loop. \n");
     TaskHandler::GetSingleton().Execute();
-    
+
     delete TankHandler::GetSingletonPtr();
     delete LevelHandler::GetSingletonPtr();
     delete FXHandler::GetSingletonPtr();
-    
+
     delete TaskHandler::GetSingletonPtr();
 }
-
 
 int main(int argc, char *argv[])
 {
     new App();
     App::GetSingleton().Run(argc, argv);
     delete App::GetSingletonPtr();
-    
+
     return 0;
 }
