4dcb389b56640e99dd4a268912b9210559d8d3d7
Brien Smith Martinez <bsmartinez@gmail.com>
2025-06-09 18:09:44 +0200

    Add Wall flag to compiler arguments

--- src/build.sh
+++ src/build.sh
@@ -3,6 +3,6 @@
 EXECUTABLE_NAME="tankgame-linux"
 
 echo "Starting build, please wait..."
-g++ -std=c++14 -o $EXECUTABLE_NAME *.cpp -lSDL2 -lSDL2_mixer -lSDL2_ttf -lGL -lGLU -ldl -lassimp
+g++ -std=c++14 -Wall -o $EXECUTABLE_NAME *.cpp -lSDL2 -lSDL2_mixer -lSDL2_ttf -lGL -lGLU -ldl -lassimp
 mv ./$EXECUTABLE_NAME ../runtime/
 echo "Build complete! Executable: ../runtime/$EXECUTABLE_NAME"
