8b3c1ff5fef6ebc911c9aa29bf51a6e22d62aeac
Brien Smith Martinez <bsmartinez@gmail.com>
2025-07-19 19:58:23 +0200

    Update build shell script to build source code files in subdirectories and build from inside development distrobox

--- src/build.sh
+++ src/build.sh
@@ -2,7 +2,26 @@
 
 EXECUTABLE_NAME="tankgame-linux"
 
-echo "Starting build, please wait..."
-g++ -std=c++14 -Wall -o $EXECUTABLE_NAME *.cpp -lSDL2 -lSDL2_mixer -lSDL2_ttf -lGL -lGLU -ldl -lassimp
-mv ./$EXECUTABLE_NAME ../runtime/
-echo "Build complete! Executable: ../runtime/$EXECUTABLE_NAME"
+echo "Starting build from cpp-dev distrobox environment..."
+
+# Run the build inside the cpp-dev distrobox container
+flatpak-spawn --host distrobox enter cpp-dev -- bash -c "
+    cd /home/deck/Dropbox/work/Programming/tankgame/src
+    
+    echo 'Finding all .cpp files in current directory and subdirectories...'
+    CPP_FILES=\$(find . -name '*.cpp' -type f)
+    
+    echo 'Compiling the following files:'
+    echo \"\$CPP_FILES\"
+    echo ''
+    
+    g++ -std=c++14 -Wall -o $EXECUTABLE_NAME \$CPP_FILES -lSDL2 -lSDL2_mixer -lSDL2_ttf -lGL -lGLU -ldl -lassimp
+    
+    if [ \$? -eq 0 ]; then
+        mv ./$EXECUTABLE_NAME ../runtime/
+        echo 'Build complete! Executable: ../runtime/$EXECUTABLE_NAME'
+    else
+        echo 'Build failed!'
+        exit 1
+    fi
+"
