1e95e71394f163fb33302d441886b4a5dac7b84c
Brien Smith Martinez <bsmartinez@gmail.com>
2025-07-22 18:01:49 +0200

    Remove build script

--- src/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-EXECUTABLE_NAME="tankgame-linux"
-
-echo "Starting build, please wait..."
-g++ -std=c++14 -Wall -o $EXECUTABLE_NAME *.cpp -lSDL2 -lSDL2_mixer -lSDL2_ttf -lGL -lGLU -ldl -lassimp
-mv ./$EXECUTABLE_NAME ../runtime/
-echo "Build complete! Executable: ../runtime/$EXECUTABLE_NAME"
